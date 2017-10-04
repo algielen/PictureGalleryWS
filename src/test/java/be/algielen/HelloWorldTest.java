28497352adf5e0fc568bc22b4c1813049ccce46c
Alexandre Gielen <alexandre.gielen@student.vinci.be>
2017-10-04 18:06:12 +0200

    Initial commit

---
+++ src/test/java/be/algielen/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package be.algielen;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+    @Test
+    void obvious() {
+        assertEquals(2, 1+1);
+    }
+}
